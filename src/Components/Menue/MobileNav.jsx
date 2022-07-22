b987b1e11d6c498d269baef8a78d753bd51f1ed8
MostafaMofeed67 <mustafamufeed22@gmail.com>
2022-07-22 06:19:24 +0200

    CU-37pxuhv - integrate user profile api

--- src/Components/Menue/MobileNav.jsx
+++ src/Components/Menue/MobileNav.jsx
@@ -28,13 +28,13 @@ const MobileNav = ({ isOpen, toggleDrawer }) => {
         <div>
           <div className="flex gap-x-5 items-center">
             <label
-              htmlFor="default-toggle"
+              htmlFor="default-toggle2"
               className="inline-flex relative items-center cursor-pointer"
             >
               <input
                 type="checkbox"
                 value=""
-                id="default-toggle"
+                id="default-toggle2"
                 className="sr-only peer"
               />
               <div className="w-11 h-6 bg-gray-200 peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-blue-300 dark:peer-focus:ring-primary/20 rounded-full peer dark:bg-gray-300 peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-primary"></div>
