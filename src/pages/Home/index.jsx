8d77504d8e45b5a7b03921085439f07310f10f61
Hossam <56669856+HOSS11H@users.noreply.github.com>
2022-07-25 18:09:01 +0200

    remove content from Home

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,21 +1,9 @@
 import React from "react";
 import Layout from "../../Components/Layout";
-import GraphCards from "./GraphCards";
-import LatesVideo from "./LatesVideo";
-import UploadedCamera from "./UploadedCamera";
 
 const Index = () => {
   return (
     <Layout>
-      <GraphCards />
-      <div className="main-container grid grid-cols-1 lg:grid-cols-3 gap-4 pb-4">
-        <div className="lg:col-span-2">
-          <LatesVideo />
-        </div>
-        <div>
-          <UploadedCamera />
-        </div>
-      </div>
     </Layout>
   );
 };
