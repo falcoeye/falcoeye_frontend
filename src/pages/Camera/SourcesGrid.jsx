e375ec6e2dcdefc2cfc51ff659eb08c0df82ae60
MostafaMofeed67 <mustafamufeed22@gmail.com>
2022-07-27 13:23:20 +0200

    CU-37pxv2m - integrate CRUD for camera resource

--- src/pages/Camera/SourcesGrid.jsx
+++ src/pages/Camera/SourcesGrid.jsx
@@ -1,56 +1,65 @@
-import { Fragment, useCallback, useState } from "react";
-import { useSelector } from "react-redux";
-import Loader from "../../Components/UI/Loader/Loader";
-import EditSource from "../Modals/EditSource";
-import SourceCard from "./SourceCard";
-import SourceShow from "./SourceShow";
-import noDataAnimation from '../../assets/animations/no-data.json'
 import Lottie from "lottie-react";
+import { Fragment } from "react";
+import { useSelector } from "react-redux";
+import noDataAnimation from "../../assets/animations/no-data.json";
+import Loader from "../../Components/UI/Loader/Loader";
+import SourceCard from "./SourceCard";
 
-const SourcesGrid = props => {
+const SourcesGrid = ({ onOpenSourceModal }) => {
+  //   const [selectedCardId, setSelectedCardId] = useState(null);
+  //   const [showModalOpened, setShowModalOpened] = useState(false);
+  //   const [editModalOpened, setEditModalOpened] = useState(false);
+  //   const closeShowModalHandler = useCallback(() => {
+  //     setShowModalOpened(false);
+  //     setSelectedCardId(null);
+  //   }, []);
+  //   const openEditModalHandler = useCallback(() => {
+  //     setShowModalOpened(false);
+  //     setEditModalOpened(true);
+  //   }, []);
+  //   const closeEditModalHandler = useCallback(() => {
+  //     setEditModalOpened(false);
+  //   }, []);
 
-    const [ selectedCardId, setSelectedCardId ] = useState(null)
-    const [ showModalOpened, setShowModalOpened] = useState(false)
-    const [ editModalOpened, setEditModalOpened ] = useState(false)
+  const sources = useSelector((state) => state.sources);
 
-    const sources = useSelector((state) => state.sources);
-
-
-    const openShowModalHandler =  useCallback( ( id ) => {
-        setShowModalOpened(true)
-        setSelectedCardId(id)
-    }, [])
-    const closeShowModalHandler =  useCallback( ( ) => {
-        setShowModalOpened(false)
-        setSelectedCardId(null)
-    }, [])
-    const openEditModalHandler = useCallback(() => {
-        setShowModalOpened(false)
-        setEditModalOpened(true);
-    }, []);
-    const closeEditModalHandler = useCallback(() => {
-        setEditModalOpened(false);
-    }, []);
-
-    if (sources.fetchingSources) {
-        return <Loader height='96' />
-    }
-    if (sources.data.length === 0 && !sources.fetchingSources) {
-        return <div className='h-96' ><Lottie animationData={noDataAnimation} loop={true}  style={{ 'width': '100%', 'height': '100%' }} /></div>
-    }
-
-    const sourcesCards = sources.data.map(source => {
-        return <SourceCard key={source.id} source={source} handleClick={openShowModalHandler} />
-    })
-
+  if (sources.fetchingSources) {
+    return <Loader height="96" />;
+  }
+  if (sources.data.length === 0 && !sources.fetchingSources) {
     return (
-        <Fragment>
-            <div className="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 gap-4">
-                {sourcesCards}
-            </div>
-            <SourceShow open={showModalOpened} handleClose={closeShowModalHandler} openEditModalHandler={openEditModalHandler} id={selectedCardId} />
-            <EditSource open={editModalOpened} handleClose={closeEditModalHandler} id={selectedCardId} />
-        </Fragment>
-    )
-}
+      <div className="h-96">
+        <Lottie
+          animationData={noDataAnimation}
+          loop={true}
+          style={{ width: "100%", height: "100%" }}
+        />
+      </div>
+    );
+  }
+
+  const sourcesCards = sources.data.map((source) => {
+    return (
+      <SourceCard
+        key={source.id}
+        source={source}
+        handleClick={onOpenSourceModal}
+      />
+    );
+  });
+
+  return (
+    <Fragment>
+      <div className="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 gap-4">
+        {sourcesCards}
+      </div>
+      {/* <SourceShow
+        open={showModalOpened}
+        handleClose={closeShowModalHandler}
+        openEditModalHandler={onOpenEditModal}
+        id={selectedCardId}
+      /> */}
+    </Fragment>
+  );
+};
 export default SourcesGrid;
---
+++ src/pages/Modals/SliderModal.jsx
@@ -0,0 +1,80 @@
+import { Dialog, Transition } from "@headlessui/react";
+import { Fragment, useState } from "react";
+import "./Modals.css";
+
+const SliderModal = ({ handleClose, open, onTriggerCapture }) => {
+  const [sliderInput, setSliderInput] = useState(10);
+
+  const changeSliderInputHandler = (e) => {
+    setSliderInput(e.target.value);
+  };
+
+  return (
+    <Transition appear show={open} as={Fragment}>
+      <Dialog as="div" className="relative z-[300]" onClose={handleClose}>
+        <Transition.Child
+          as={Fragment}
+          enter="ease-out duration-300"
+          enterFrom="opacity-0"
+          enterTo="opacity-100"
+          leave="ease-in duration-200"
+          leaveFrom="opacity-100"
+          leaveTo="opacity-0"
+        >
+          <div className="fixed inset-0 bg-black bg-opacity-25" />
+        </Transition.Child>
+
+        <div className="fixed inset-0 overflow-y-auto">
+          <div className="flex min-h-full items-center justify-center p-4 text-center">
+            <Transition.Child
+              as={Fragment}
+              enter="ease-out duration-300"
+              enterFrom="opacity-0 scale-95"
+              enterTo="opacity-100 scale-100"
+              leave="ease-in duration-200"
+              leaveFrom="opacity-100 scale-100"
+              leaveTo="opacity-0 scale-95"
+            >
+              <Dialog.Panel className="w-full max-w-md  transform overflow-hidden rounded-2xl bg-white p-6 text-left align-middle shadow-xl transition-all">
+                <div className="text-[#42a7df] text-lg font-semibold text-center">
+                  Select Capture Length
+                </div>
+
+                <div className="flex justify-end mt-2">
+                  <label
+                    htmlFor="steps-range"
+                    className="block mb-2 text-sm font-semibold text-white bg-primary w-fit p-2 rounded-lg "
+                  >
+                    {sliderInput}
+                  </label>
+                </div>
+                <input
+                  id="steps-range"
+                  type="range"
+                  min="10"
+                  max="120"
+                  value={sliderInput}
+                  step="10"
+                  className="w-full h-2 bg-gray-200 rounded-lg appearance-none cursor-pointer text-primary"
+                  onChange={changeSliderInputHandler}
+                ></input>
+                <button
+                  onClick={() => {
+                    handleClose();
+                    onTriggerCapture();
+                  }}
+                  type="button"
+                  className="focus:outline-none text-white bg-green/70 hover:bg-green focus:ring-4 focus:ring-green/30 font-medium rounded-lg text-sm px-5 py-2.5 mt-5"
+                >
+                  Submit
+                </button>
+              </Dialog.Panel>
+            </Transition.Child>
+          </div>
+        </div>
+      </Dialog>
+    </Transition>
+  );
+};
+
+export default SliderModal;
