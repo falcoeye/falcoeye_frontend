7f979eba67c1778ebe98d3440743fea0efe1b495
MostafaMofeed67 <mustafamufeed22@gmail.com>
2022-07-31 12:58:25 +0200

    CU-37pxyfr - feature, CRUD of analysis

--- src/pages/Modals/components/steps/Name.js
+++ src/pages/Modals/components/steps/Name.js
@@ -1,12 +1,14 @@
-const Name = () => {
+const Name = ({ onAnalysisNameChange, analysisName }) => {
   return (
     <div className="flex flex-col ">
       <div className="mx-2 w-full flex-1">
         <div className="my-2 flex rounded border border-gray-200 bg-white p-1">
           <input
             name="name"
-            placeholder="Anaylsis Name"
+            placeholder="Anaylsis Name *"
             className="w-full appearance-none p-1 px-2 text-gray-800 outline-none"
+            onChange={(e) => onAnalysisNameChange(e.target.value)}
+            value={analysisName}
           />
         </div>
       </div>
