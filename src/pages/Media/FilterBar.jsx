7e74e68baa89a5d41eef1c709c31eb37459bc9be
Hossam <56669856+HOSS11H@users.noreply.github.com>
2022-08-03 03:17:03 +0200

    CU-39g82ca - Feature, Integrate Media API

--- src/pages/Media/FilterBar.jsx
+++ src/pages/Media/FilterBar.jsx
@@ -1,92 +1,99 @@
 import React from "react";
 import { AiOutlineSearch } from "react-icons/ai";
-import AutoComplete from "../../Components/AutoComplete";
-import { BsFillGrid3X2GapFill } from "react-icons/bs";
-import { AiOutlineUnorderedList } from "react-icons/ai";
 
-function FilterBar({ setSort, sort }) {
+const FilterBar = ({
+  onChangeDataType,
+  onChangeDataOrder,
+  dataType,
+  dataOrder,
+  onChangeSearchInput,
+  searchInput,
+}) => {
+  const inputSearchChangeHandler = (e) => {
+    onChangeSearchInput(e.target.value);
+  };
+
+  const selectOrderHandler = (e) => {
+    onChangeDataOrder(e.target.value);
+  };
+  const selectOrderTypeHandler = (e) => {
+    onChangeDataType(e.target.value);
+  };
+
   return (
-    <div className="main-container ">
-      <div className="bg-white px-7 mx-1">
-        <div className=" bg-backgroundLight flex items-center md:px-4 pt-2 rounded-md">
-          <div className="flex  gap-2 md:flex-row flex-col w-full">
-            <div className="flex items-center gap-2 md:flex-row flex-col justify-between flex-[2] md:order-none order-last">
-              <div className="md:px-0 flex-1 px-4 w-full">
-                <div className=" w-full md:mt-0 mt-2 mb-2 py-1  flex items-center px-4 rounded-full   bg-white md:max-w-[320px] max-w-full  ">
-                  <span className="text-primary text-xl mr-4 ">
-                    <AiOutlineSearch />
-                  </span>
-                  <AutoComplete
-                    data={suggestions}
-                    placeholder={`Type into search ...`}
-                  />
-                </div>
-              </div>
-              <div className="flex flex-1  md:mt-0 mt-2 md:pt-0 pt-2 pb-2 md:bg-backgroundLight bg-white  gap-2 md:w-auto items-center w-full justify-end md:order-none order-last ">
-                <div
-                  onClick={() => setSort("grid")}
-                  className={`${
-                    sort === "grid"
-                      ? "bg-primary text-white"
-                      : "bg-white text-primary"
-                  } rounded-full cursor-pointer hover:bg-primary p-3 hover:text-white`}
-                >
-                  <BsFillGrid3X2GapFill className="  " />
-                </div>
-                <div
-                  onClick={() => setSort("list")}
-                  className={`${
-                    sort === "list"
-                      ? "bg-primary text-white"
-                      : "bg-white text-primary"
-                  } rounded-full cursor-pointer hover:bg-primary p-3 hover:text-white`}
-                >
-                  <AiOutlineUnorderedList className=" " />
-                </div>
+    <div className=" bg-backgroundLight flex items-center md:px-4 pt-2 rounded-md">
+      <div className="flex  md:gap-4 justify-between md:flex-row flex-col w-full">
+        <div className="flex items-center gap-2 md:flex-row flex-col justify-between  md:order-none order-last">
+          <div className="md:px-0 px-4 w-full">
+            <div className=" md:w-auto  w-full md:mt-0 mt-2 mb-2 py-1  flex items-center px-4 rounded-full   bg-white md:max-w-[270px] max-w-full  ">
+              <span className="text-primary text-xl mr-4 ">
+                <AiOutlineSearch />
+              </span>
+              <div className="w-full relative">
+                <input
+                  className="focus:outline-none text-primary placeholder-primary text-sm bg-transparent w-full"
+                  type="text"
+                  value={searchInput}
+                  onChange={inputSearchChangeHandler}
+                />
               </div>
             </div>
-            <div className="flex flex-1 w-full gap-4 md:flex-row flex-col items-center mb-2 md:px-0 px-4">
-              <div className="flex min-w-max items-center w-full flex-1 md:max-w-sm bg-white px-3 rounded-md">
-                <span
-                  htmlFor="location"
-                  className="block text-sm font-medium text-primary whitespace-nowrap"
-                >
-                  Sort by:
-                </span>
-                <select
-                  id="location"
-                  name="location"
-                  className="mt-1 block  bg-white  w-full pl-1  py-2 text-base  focus:outline-none  sm:text-sm rounded-md"
-                >
-                  <option>Model Name</option>
-                  <option>Canada</option>
-                  <option>Mexico</option>
-                </select>
-              </div>
-              <div className="flex flex-1 w-full items-center min-w-max bg-white px-3 rounded-md">
-                <span
-                  htmlFor="location"
-                  className="block text-sm font-medium text-primary whitespace-nowrap"
-                >
-                  Sort by:
-                </span>
-                <select
-                  id="location"
-                  name="location"
-                  className="mt-1 block bg-white  w-full py-2 text-base  focus:outline-none  sm:text-sm rounded-md"
-                >
-                  <option>A-Z</option>
-                  <option>Canada</option>
-                  <option>Mexico</option>
-                </select>
-              </div>
-            </div>
+          </div>
+        </div>
+        <div className="flex gap-4 md:flex-row flex-col items-center mb-2 md:px-0 px-4 w-full md:max-w-md">
+          <div className="flex items-center md:max-w-lg w-full   bg-white  px-3 rounded-md">
+            <span
+              htmlFor="type"
+              className="block text-sm font-medium text-primary whitespace-nowrap"
+            >
+              Sort by:
+            </span>
+            <select
+              id="type"
+              name="type"
+              className="cursor-pointer block bg-white  w-full pl-1  py-2 text-base  focus:outline-none  sm:text-sm rounded-md"
+              onChange={selectOrderTypeHandler}
+              value={dataType}
+            >
+              <option>Title</option>
+              <option>Creator</option>
+              <option>Date</option>
+            </select>
+          </div>
+          <div className="flex items-center md:max-w-lg w-full bg-white  px-3 rounded-md">
+            <span
+              htmlFor="order"
+              className="block text-sm font-medium text-primary whitespace-nowrap"
+            >
+              Sort by:
+            </span>
+            <select
+              id="order"
+              name="order"
+              className="cursor-pointer block bg-white  w-full pl-1  py-2 text-base  focus:outline-none  sm:text-sm rounded-md"
+              onChange={selectOrderHandler}
+              value={dataOrder || ""}
+            >
+              <option disabled value="">
+                Choose Order
+              </option>
+              {dataType === "Date" ? (
+                <>
+                  <option value="oldest">Oldest</option>
+                  <option value="newest">Newest</option>
+                </>
+              ) : (
+                <>
+                  <option value="a-z">A-Z</option>
+                  <option value="z-a">Z-A</option>
+                </>
+              )}
+            </select>
           </div>
         </div>
       </div>
     </div>
   );
-}
+};
 
 export default FilterBar;
-const suggestions = ["aaa", "sasdf", "askdf"];
--- src/pages/Media/MediaView.jsx
+++ src/pages/Media/MediaView.jsx
@@ -4,10 +4,16 @@ import { fetchMedia } from '../../store/media';
 import MediaGrid from './MediaGrid';
 import { BsFillGrid1X2Fill, BsListUl } from 'react-icons/bs';
 import MediaList from './MediaList';
+import FilterBar from './FilterBar';
 
 const MediaView = (props) => {
   const dispatch = useDispatch();
   const [isGridView, setIsGridView] = useState(true);
+
+  const [ term, setTerm ] = useState('');
+  const [ type, setType ] = useState('');
+  const [ date, setDate ] = useState('');
+  const [ order, setOrder ] = useState('')
 
   useEffect(() => {
     dispatch(fetchMedia());
@@ -18,7 +24,7 @@ const MediaView = (props) => {
       <div>
         <div className="main-container mt-6 ">
           <div className=" bg-white mx-1 pt-4 px-4 md:px-7 pb-7 rounded-sm">
-            <div className="flex justify-between sm:flex-row flex-col">
+            <div className="flex justify-between sm:flex-row flex-co pb-4 mb-5 border-b border-[#f5f5f5] ">
               <h2 className="text-gray-900 text-lg capitalize">media</h2>
               <div className="flex">
                 <button
@@ -39,6 +45,7 @@ const MediaView = (props) => {
                 </button>
               </div>
             </div>
+            <FilterBar />
             <div className="mt-6 rounded-xl bg-white p-3 md:p-6 shadow">
                 { isGridView ? <MediaGrid /> : <MediaList />  }
             </div>
