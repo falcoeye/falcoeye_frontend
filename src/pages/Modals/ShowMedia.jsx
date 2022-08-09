08abae9d6c2d0669cb2d21974b0a616e583f706c
Hossam <56669856+HOSS11H@users.noreply.github.com>
2022-08-09 02:48:18 +0200

    Add original image preview to the show media modal

--- src/pages/Modals/ShowMedia.jsx
+++ src/pages/Modals/ShowMedia.jsx
@@ -1,19 +1,25 @@
 import { Dialog, Transition } from '@headlessui/react';
 import Lottie from 'lottie-react';
-import React, { Fragment, useCallback, useState } from 'react';
+import React, { Fragment, useCallback, useEffect, useState } from 'react';
 import {
 	AiFillCamera,
-	AiFillVideoCamera, AiOutlineClose
+	AiFillVideoCamera,
+	AiOutlineClose,
 } from 'react-icons/ai';
 import { MdDelete } from 'react-icons/md';
 import { useSelector } from 'react-redux';
+import { toast } from 'react-toastify';
 import noDataAnimation from '../../assets/animations/no-data.json';
 import DeleteMedia from './DeleteMedia';
 import './Modals.css';
+import axios from '../../utility/api-instance';
+import { useRef } from 'react';
 
 const ShowMedia = ({ open, handleClose, id }) => {
 	const media = useSelector((state) => state.media);
+	let data = useRef(null);
 	const [deleteModalOpened, setDeleteModalOpened] = useState(false);
+	const [mediaPreview, setMediaPreview] = useState(null);
 
 	const openDeleteModalHandler = useCallback(
 		() => setDeleteModalOpened(true),
@@ -24,15 +30,44 @@ const ShowMedia = ({ open, handleClose, id }) => {
 		[]
 	);
 
-	let data;
+	const fetchMediaPreview = useCallback((id, mediaType) => {
+		let url = `media/image/${id}/img_original.jpg`;
+		let type = { responseType: 'blob' }
+		if (mediaType === 'video') {
+			url = `media/video/${id}/video_original.mp4`;
+			type = {}
+		}
+		axios
+			.get(url, type)
+			.then((res) => {
+				if ( mediaType === 'image' ) {
+					// we can all pass them to the Blob constructor directly
+					const new_blob = new Blob([res.data], { type: 'image/jpg' });
+					const url = URL.createObjectURL(new_blob);
+					setMediaPreview(url);
+					return;
+				} 
+				setMediaPreview(res.data);
+			})
+			.catch((err) => {
+				toast.error(err.response.data.message);
+			});
+	}, []);
+
+	useEffect(() => {
+		if (id && data.current) {
+			fetchMediaPreview(id, data.current.media_type)
+		}
+	}, [id, fetchMediaPreview])
+
 
 	if (id) {
-		data = media.data.find((item) => item.id === id);
+		data.current = media.data.find((item) => item.id === id);
 	}
 
 	let content;
 
-	if (id && !data) {
+	if (id && !data.current) {
 		content = (
 			<Fragment>
 				<div className="flex justify-end gap-5">
@@ -54,7 +89,27 @@ const ShowMedia = ({ open, handleClose, id }) => {
 		);
 	}
 
-	if (id && data) {
+	if (id && data.current) {
+		let renderedPreview = (
+			<div className="flex justify-center items-center h-96 bg-gray-300 mb-3">
+				<svg
+					className="w-12 h-12 text-gray-200"
+					xmlns="http://www.w3.org/2000/svg"
+					aria-hidden="true"
+					fill="currentColor"
+					viewBox="0 0 640 512"
+				>
+					<path d="M480 80C480 35.82 515.8 0 560 0C604.2 0 640 35.82 640 80C640 124.2 604.2 160 560 160C515.8 160 480 124.2 480 80zM0 456.1C0 445.6 2.964 435.3 8.551 426.4L225.3 81.01C231.9 70.42 243.5 64 256 64C268.5 64 280.1 70.42 286.8 81.01L412.7 281.7L460.9 202.7C464.1 196.1 472.2 192 480 192C487.8 192 495 196.1 499.1 202.7L631.1 419.1C636.9 428.6 640 439.7 640 450.9C640 484.6 612.6 512 578.9 512H55.91C25.03 512 .0006 486.1 .0006 456.1L0 456.1z" />
+				</svg>
+			</div>
+		)
+		if ( mediaPreview && data.current.media_type === 'image' ) {
+			renderedPreview = (
+				<div className="flex justify-center items-center h-96 bg-gray-300 mb-3">
+					<img src ={mediaPreview} alt={data.current.media_type} className="w-full h-full object-cover"  />
+				</div>
+			)
+		}
 		content = (
 			<Fragment>
 				<div className="flex justify-end gap-5 mb-5">
@@ -71,17 +126,7 @@ const ShowMedia = ({ open, handleClose, id }) => {
 						<AiOutlineClose />
 					</button>
 				</div>
-				<div className="flex justify-center items-center h-96 bg-gray-300 mb-3">
-					<svg
-						className="w-12 h-12 text-gray-200"
-						xmlns="http://www.w3.org/2000/svg"
-						aria-hidden="true"
-						fill="currentColor"
-						viewBox="0 0 640 512"
-					>
-						<path d="M480 80C480 35.82 515.8 0 560 0C604.2 0 640 35.82 640 80C640 124.2 604.2 160 560 160C515.8 160 480 124.2 480 80zM0 456.1C0 445.6 2.964 435.3 8.551 426.4L225.3 81.01C231.9 70.42 243.5 64 256 64C268.5 64 280.1 70.42 286.8 81.01L412.7 281.7L460.9 202.7C464.1 196.1 472.2 192 480 192C487.8 192 495 196.1 499.1 202.7L631.1 419.1C636.9 428.6 640 439.7 640 450.9C640 484.6 612.6 512 578.9 512H55.91C25.03 512 .0006 486.1 .0006 456.1L0 456.1z" />
-					</svg>
-				</div>
+				{renderedPreview}
 				{/* <h5 className="mb-3 text-xl font-semibold tracking-tight text-gray-900  capitalize ">
 					{data.note}
 				</h5> */}
@@ -89,18 +134,18 @@ const ShowMedia = ({ open, handleClose, id }) => {
 					className={`inline-flex items-center mb-3 py-1 px-2 text-base font-medium text-center text-white capitalize ${media.media_type === 'image' ? 'bg-sky-400' : 'bg-emerald-500'
 						} rounded-lg`}
 				>
-					{data.media_type === 'image' ? (
+					{data.current.media_type === 'image' ? (
 						<AiFillCamera className="mr-2" />
 					) : (
 						<AiFillVideoCamera className="mr-2" />
 					)}
-					{data.media_type}
+					{data.current.media_type}
 				</div>
 				<DeleteMedia
 					open={deleteModalOpened}
 					handleClose={closeDeleteModalHandler}
 					id={id}
-					type={data.media_type}
+					type={data.current.media_type}
 					handleShowClose={handleClose}
 				/>
 			</Fragment>
