af5ab1e8459a2e0bf10d905b79e473bd9c187ce8
MostafaMofeed67 <mustafamufeed22@gmail.com>
2022-08-18 04:09:12 +0200

    CU-3bumpf0 - Feature, implement Upload in Media

--- src/pages/Media/MediaView.jsx
+++ src/pages/Media/MediaView.jsx
@@ -1,25 +1,27 @@
-import { useEffect, useState } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import { fetchMedia } from '../../store/media';
-import MediaGrid from './MediaGrid';
-import { BsFillGrid1X2Fill, BsListUl } from 'react-icons/bs';
-import MediaList from './MediaList';
-import FilterBar from './FilterBar';
-import { useCallback } from 'react';
+import { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { fetchMedia } from "../../store/media";
+import MediaGrid from "./MediaGrid";
+import { BsFillGrid1X2Fill, BsListUl } from "react-icons/bs";
+import MediaList from "./MediaList";
+import FilterBar from "./FilterBar";
+import { useCallback } from "react";
+import UploadMedia from "../Modals/UploadMedia";
 
 const MediaView = (props) => {
   const dispatch = useDispatch();
   const [isGridView, setIsGridView] = useState(true);
 
   const media = useSelector((state) => state.media);
-  const { data }= media;
+  const { data } = media;
 
   const [filteredMedia, setFilteredMedia] = useState(data);
+  const [uploadMediaImage, setUploadMediaImage] = useState(false);
 
-  const [term, setTerm] = useState('');
-  const [type, setType] = useState('all');
-  const [date, setDate] = useState('');
-  const [order, setOrder] = useState('');
+  const [term, setTerm] = useState("");
+  const [type, setType] = useState("all");
+  const [date, setDate] = useState("");
+  const [order, setOrder] = useState("");
 
   const termChangeHandler = useCallback((e) => {
     setTerm(e.target.value);
@@ -38,79 +40,106 @@ const MediaView = (props) => {
     dispatch(fetchMedia());
   }, [dispatch]);
 
-  const searchFilter = ( term, items ) => {
-    if ( term.length === 0 ) return items;
-    const searchFilteredData = items.filter( item => {
-      return item.tags.toLowerCase().includes(term.toLowerCase())
-    } )
-    return searchFilteredData
-  }
+  const searchFilter = (term, items) => {
+    if (term.length === 0) return items;
+    const searchFilteredData = items.filter((item) => {
+      return item.tags.toLowerCase().includes(term.toLowerCase());
+    });
+    return searchFilteredData;
+  };
 
-  const typeFilter = ( type, items ) => {
-    switch ( type ) {
-      case 'all' : return items
-      default : 
-        return items.filter( item => item.media_type === type)
+  const typeFilter = (type, items) => {
+    switch (type) {
+      case "all":
+        return items;
+      default:
+        return items.filter((item) => item.media_type === type);
     }
-  }
+  };
 
-  const dateFilter = ( val , items ) => {
-    switch ( val ) {
-      case 'oldest' : return items.sort((a, b) => new Date(a.created_at) - new Date(b.created_at) )
-      case 'newest' : return items.sort((a, b) => new Date(b.created_at) - new Date(a.created_at) )
-      default : return items
+  const dateFilter = (val, items) => {
+    switch (val) {
+      case "oldest":
+        return items.sort(
+          (a, b) => new Date(a.created_at) - new Date(b.created_at)
+        );
+      case "newest":
+        return items.sort(
+          (a, b) => new Date(b.created_at) - new Date(a.created_at)
+        );
+      default:
+        return items;
     }
-  }
+  };
 
-  const orderFilter = ( val, items ) => {
-    switch ( val ) {
-      case 'a-z': return items.sort((a, b) => a.note.localeCompare(b.note));
-      case 'z-a': return items.sort((a, b) => b.note.localeCompare(a.note));
-      default : return items
+  const orderFilter = (val, items) => {
+    switch (val) {
+      case "a-z":
+        return items.sort((a, b) => a.note.localeCompare(b.note));
+      case "z-a":
+        return items.sort((a, b) => b.note.localeCompare(a.note));
+      default:
+        return items;
     }
-  }
+  };
 
   const filterationHandler = useCallback(() => {
-    let filteredData = [...data]
-    const searchFilteredResults = searchFilter(term,filteredData)
-    const typeFiltersResults = typeFilter( type, searchFilteredResults )
-    const dateFiltersResults = dateFilter( date, typeFiltersResults)
-    const orderFilterResults = orderFilter( order, dateFiltersResults )
-    setFilteredMedia(orderFilterResults)
-  }, [data, date, order, term, type])
+    let filteredData = [...data];
+    const searchFilteredResults = searchFilter(term, filteredData);
+    const typeFiltersResults = typeFilter(type, searchFilteredResults);
+    const dateFiltersResults = dateFilter(date, typeFiltersResults);
+    const orderFilterResults = orderFilter(order, dateFiltersResults);
+    setFilteredMedia(orderFilterResults);
+  }, [data, date, order, term, type]);
 
-  useEffect(() =>{
-    if ( data.length > 0) {
-      filterationHandler()
-    } 
-  }, [data, filterationHandler] )
+  useEffect(() => {
+    if (data.length > 0) {
+      filterationHandler();
+    }
+  }, [data, filterationHandler]);
 
   return (
     <>
       <div>
         <div className="main-container mt-6 ">
           <div className=" bg-white mx-1 pt-4 px-4 md:px-7 pb-7 rounded-sm">
-            <div className="flex justify-between sm:flex-row flex-co pb-4 mb-5 border-b border-[#f5f5f5] ">
+            <div className="flex justify-between items-center sm:flex-row flex-co pb-4 mb-5 border-b border-[#f5f5f5] ">
               <h2 className="text-gray-900 text-lg capitalize">media</h2>
-              <div className="flex">
+              <div className="flex items-center">
+                <button
+                  type="button"
+                  onClick={() => setUploadMediaImage(true)}
+                  className="text-white bg-primary/90 hover:bg-primary focus:ring-4 mr-5 focus:ring-blue-100 font-medium rounded-lg  px-4 py-2  focus:outline-none"
+                >
+                  Upload
+                </button>
                 <button
                   onClick={() => setIsGridView(true)}
-                  className={`transition duration-300 p-2 rounded-full inline-flex items-center mr-4 text-primary ${isGridView && 'bg-primary text-white'
-                    }`}
+                  className={`transition duration-300 p-2 rounded-full inline-flex items-center mr-4 text-primary ${
+                    isGridView && "bg-primary text-white"
+                  }`}
                 >
                   <BsFillGrid1X2Fill />
                 </button>
                 <button
                   onClick={() => setIsGridView(false)}
-                  className={`transition duration-300 p-2 rounded-full inline-flex items-center text-primary ${!isGridView && 'bg-primary text-white'
-                    }`}
+                  className={`transition duration-300 p-2 rounded-full inline-flex items-center text-primary ${
+                    !isGridView && "bg-primary text-white"
+                  }`}
                 >
                   <BsListUl />
                 </button>
               </div>
             </div>
-            <FilterBar term={term} type={type} date={date} order={order}
-              termHandler={termChangeHandler} typeHandler={typeChangeHandler} dateHandler={dateChangeHandler} orderHandler={orderChangeHandler}
+            <FilterBar
+              term={term}
+              type={type}
+              date={date}
+              order={order}
+              termHandler={termChangeHandler}
+              typeHandler={typeChangeHandler}
+              dateHandler={dateChangeHandler}
+              orderHandler={orderChangeHandler}
             />
             <div className="mt-6 rounded-xl bg-white p-3 md:p-6 shadow">
               {isGridView ? (
@@ -122,6 +151,11 @@ const MediaView = (props) => {
           </div>
         </div>
       </div>
+
+      <UploadMedia
+        handleClose={() => setUploadMediaImage(false)}
+        open={uploadMediaImage}
+      />
     </>
   );
 };
--- src/pages/Modals/ShowMedia.jsx
+++ src/pages/Modals/ShowMedia.jsx
@@ -1,212 +1,224 @@
-import { Dialog, Transition } from '@headlessui/react';
-import Lottie from 'lottie-react';
-import React, { Fragment, useCallback, useEffect, useState } from 'react';
+import { Dialog, Transition } from "@headlessui/react";
+import Lottie from "lottie-react";
+import React, { Fragment, useCallback, useEffect, useState } from "react";
 import {
-	AiFillCamera,
-	AiFillVideoCamera,
-	AiOutlineClose,
-} from 'react-icons/ai';
-import { MdDelete } from 'react-icons/md';
-import { useSelector } from 'react-redux';
-import { toast } from 'react-toastify';
-import noDataAnimation from '../../assets/animations/no-data.json';
-import DeleteMedia from './DeleteMedia';
-import './Modals.css';
-import axios from '../../utility/api-instance';
-import { useRef } from 'react';
+  AiFillCamera,
+  AiFillVideoCamera,
+  AiOutlineClose,
+} from "react-icons/ai";
+import { MdDelete } from "react-icons/md";
+import { useSelector } from "react-redux";
+import { toast } from "react-toastify";
+import noDataAnimation from "../../assets/animations/no-data.json";
+import DeleteMedia from "./DeleteMedia";
+import "./Modals.css";
+import axios from "../../utility/api-instance";
+import { useRef } from "react";
 
 const ShowMedia = ({ open, handleClose, id }) => {
-	const media = useSelector((state) => state.media);
-	let data = useRef(null);
-	const [deleteModalOpened, setDeleteModalOpened] = useState(false);
-	const [mediaPreview, setMediaPreview] = useState(null);
-	const [ loading, setLoading ] = useState(false)
+  const media = useSelector((state) => state.media);
+  let data = useRef(null);
+  const [deleteModalOpened, setDeleteModalOpened] = useState(false);
+  const [mediaPreview, setMediaPreview] = useState(null);
+  const [loading, setLoading] = useState(false);
 
-	const openDeleteModalHandler = useCallback(
-		() => setDeleteModalOpened(true),
-		[]
-	);
-	const closeDeleteModalHandler = useCallback(
-		() => setDeleteModalOpened(false),
-		[]
-	);
+  console.log(mediaPreview);
 
-	const fetchMediaPreview = useCallback((id, mediaType) => {
-		let url = `media/image/${id}/img_original.jpg`;
-		let type = { responseType: 'blob' }
-		if (mediaType === 'video') {
-			url = `media/video/${id}/video_original.mp4`;
-			type = {}
-		}
-		setLoading(true)
-		axios
-			.get(url, type)
-			.then((res) => {
-				if ( mediaType === 'image' ) {
-					// we can all pass them to the Blob constructor directly
-					const new_blob = new Blob([res.data], { type: 'image/jpg' });
-					const url = URL.createObjectURL(new_blob);
-					setMediaPreview(url);
-					return;
-				}
-				setLoading(false)
-				setMediaPreview(res.data);
-			})
-			.catch((err) => {
-				setLoading(false)
-				console.log(err)
-				toast.error(err.response.data.message);
-			});
-	}, []);
+  const openDeleteModalHandler = useCallback(
+    () => setDeleteModalOpened(true),
+    []
+  );
+  const closeDeleteModalHandler = useCallback(
+    () => setDeleteModalOpened(false),
+    []
+  );
 
-	useEffect(() => {
-		if (id && data.current) {
-			fetchMediaPreview(id, data.current.media_type)
-		}
-	}, [id, fetchMediaPreview])
+  const fetchMediaPreview = useCallback((id, mediaType) => {
+    let url = `media/image/${id}/img_original.jpg`;
+    let type = { responseType: "blob" };
+    if (mediaType === "video") {
+      url = `media/video/${id}/video_original.mp4`;
+      type = {};
+    }
+    setLoading(true);
+    axios
+      .get(url, type)
+      .then((res) => {
+        if (mediaType === "image") {
+          // we can all pass them to the Blob constructor directly
+          const new_blob = new Blob([res.data], { type: "image/jpg" });
+          const url = URL.createObjectURL(new_blob);
+          setMediaPreview(url);
+          return;
+        }
+        setLoading(false);
+        setMediaPreview(res.data);
+      })
+      .catch((err) => {
+        setLoading(false);
+        console.log(err);
+        toast.error(err.response.data.message);
+      });
+  }, []);
 
+  useEffect(() => {
+    if (id && data.current) {
+      fetchMediaPreview(id, data.current.media_type);
+    }
+  }, [id, fetchMediaPreview]);
 
-	if (id) {
-		data.current = media.data.find((item) => item.id === id);
-	}
+  if (id) {
+    data.current = media.data.find((item) => item.id === id);
+  }
 
-	let content;
+  let content;
 
-	if (id && !data.current) {
-		content = (
-			<Fragment>
-				<div className="flex justify-end gap-5">
-					<button
-						className="bg-gray-50 hover:bg-gray-200 transition duration-300 font-bold p-2 rounded-full inline-flex items-center"
-						onClick={handleClose}
-					>
-						<AiOutlineClose />
-					</button>
-				</div>
-				<div className="h-96">
-					<Lottie
-						animationData={noDataAnimation}
-						loop={true}
-						style={{ width: '100%', height: '100%' }}
-					/>
-				</div>
-			</Fragment>
-		);
-	}
+  if (id && !data.current) {
+    content = (
+      <Fragment>
+        <div className="flex justify-end gap-5">
+          <button
+            className="bg-gray-50 hover:bg-gray-200 transition duration-300 font-bold p-2 rounded-full inline-flex items-center"
+            onClick={handleClose}
+          >
+            <AiOutlineClose />
+          </button>
+        </div>
+        <div className="h-96">
+          <Lottie
+            animationData={noDataAnimation}
+            loop={true}
+            style={{ width: "100%", height: "100%" }}
+          />
+        </div>
+      </Fragment>
+    );
+  }
 
-	if (id && data.current) {
-		let renderedPreview = (
-			<div className={`flex justify-center items-center h-96 bg-gray-300 mb-3 ${loading && 'animate-pulse'}`}>
-				<svg
-					className="w-12 h-12 text-gray-200"
-					xmlns="http://www.w3.org/2000/svg"
-					aria-hidden="true"
-					fill="currentColor"
-					viewBox="0 0 640 512"
-				>
-					<path d="M480 80C480 35.82 515.8 0 560 0C604.2 0 640 35.82 640 80C640 124.2 604.2 160 560 160C515.8 160 480 124.2 480 80zM0 456.1C0 445.6 2.964 435.3 8.551 426.4L225.3 81.01C231.9 70.42 243.5 64 256 64C268.5 64 280.1 70.42 286.8 81.01L412.7 281.7L460.9 202.7C464.1 196.1 472.2 192 480 192C487.8 192 495 196.1 499.1 202.7L631.1 419.1C636.9 428.6 640 439.7 640 450.9C640 484.6 612.6 512 578.9 512H55.91C25.03 512 .0006 486.1 .0006 456.1L0 456.1z" />
-				</svg>
-			</div>
-		)
-		if ( mediaPreview && data.current.media_type === 'image' ) {
-			renderedPreview = (
-				<div className="flex justify-center items-center h-96 bg-gray-300 mb-3">
-					<img src ={mediaPreview} alt={data.current.media_type} className="w-full h-full object-cover"  />
-				</div>
-			)
-		}
-		if ( mediaPreview && data.current.media_type === 'video' ) {
-			console.log(mediaPreview)
-			renderedPreview = (
-				<div className="flex justify-center items-center h-96 bg-gray-300 mb-3">
-					<video className="w-full h-full object-cover block" controls>
-						<source src={mediaPreview} type="video/mp4"/>
-						Your browser does not support the video tag.
-					</video>
-				</div>
-			)
-		}
-		content = (
-			<Fragment>
-				<div className="flex justify-end gap-5 mb-5">
-					<button
-						className="bg-red-600/90 hover:bg-red-600 text-white transition duration-300 font-bold p-2 rounded-full inline-flex items-center"
-						onClick={openDeleteModalHandler}
-					>
-						<MdDelete />
-					</button>
-					<button
-						className="bg-gray-50 hover:bg-gray-200 transition duration-300 font-bold p-2 rounded-full inline-flex items-center"
-						onClick={handleClose}
-					>
-						<AiOutlineClose />
-					</button>
-				</div>
-				{renderedPreview}
-				<div
-					className={`inline-flex items-center mb-3 py-1 px-2 text-base font-medium text-center text-white capitalize ${media.media_type === 'image' ? 'bg-sky-400' : 'bg-emerald-500'
-						} rounded-lg`}
-				>
-					{data.current.media_type === 'image' ? (
-						<AiFillCamera className="mr-2" />
-					) : (
-						<AiFillVideoCamera className="mr-2" />
-					)}
-					{data.current.media_type}
-				</div>
-				<h5 className="mb-2 text-xl font-semibold tracking-tight text-gray-900  capitalize ">
-					{data.current.note}
-				</h5>
-				<div className="flex">
-					<span className="inline-flex items-center py-1 px-2 text-sm justify-center text-white capitalize bg-orange-500 rounded-md">
-						{data.current.tags}
-					</span>
-					</div>
-				<DeleteMedia
-					open={deleteModalOpened}
-					handleClose={closeDeleteModalHandler}
-					id={id}
-					type={data.current.media_type}
-					handleShowClose={handleClose}
-				/>
-			</Fragment>
-		);
-	}
+  if (id && data.current) {
+    let renderedPreview = (
+      <div
+        className={`flex justify-center items-center h-96 bg-gray-300 mb-3 ${
+          loading && "animate-pulse"
+        }`}
+      >
+        <svg
+          className="w-12 h-12 text-gray-200"
+          xmlns="http://www.w3.org/2000/svg"
+          aria-hidden="true"
+          fill="currentColor"
+          viewBox="0 0 640 512"
+        >
+          <path d="M480 80C480 35.82 515.8 0 560 0C604.2 0 640 35.82 640 80C640 124.2 604.2 160 560 160C515.8 160 480 124.2 480 80zM0 456.1C0 445.6 2.964 435.3 8.551 426.4L225.3 81.01C231.9 70.42 243.5 64 256 64C268.5 64 280.1 70.42 286.8 81.01L412.7 281.7L460.9 202.7C464.1 196.1 472.2 192 480 192C487.8 192 495 196.1 499.1 202.7L631.1 419.1C636.9 428.6 640 439.7 640 450.9C640 484.6 612.6 512 578.9 512H55.91C25.03 512 .0006 486.1 .0006 456.1L0 456.1z" />
+        </svg>
+      </div>
+    );
+    if (mediaPreview && data.current.media_type === "image") {
+      renderedPreview = (
+        <div className="flex justify-center items-center h-96 bg-gray-300 mb-3">
+          <img
+            src={mediaPreview}
+            alt={data.current.media_type}
+            className="w-full h-full object-cover"
+          />
+        </div>
+      );
+    }
+    if (mediaPreview && data.current.media_type === "video") {
+      console.log(mediaPreview);
+      renderedPreview = (
+        <div className="flex justify-center items-center h-96 bg-gray-300 mb-3">
+          <video className="w-full h-full object-cover block" controls>
+            <source src={mediaPreview} type="video/mp4" />
+            Your browser does not support the video tag.
+          </video>
+        </div>
+      );
+    }
+    content = (
+      <Fragment>
+        <div className="flex justify-end gap-5 mb-5">
+          <button
+            className="bg-red-600/90 hover:bg-red-600 text-white transition duration-300 font-bold p-2 rounded-full inline-flex items-center"
+            onClick={openDeleteModalHandler}
+          >
+            <MdDelete />
+          </button>
+          <button
+            className="bg-gray-50 hover:bg-gray-200 transition duration-300 font-bold p-2 rounded-full inline-flex items-center"
+            onClick={handleClose}
+          >
+            <AiOutlineClose />
+          </button>
+        </div>
+        {renderedPreview}
+        <div
+          className={`inline-flex items-center mb-3 py-1 px-2 text-base font-medium text-center text-white capitalize ${
+            data.current.media_type === "image"
+              ? "bg-sky-400"
+              : "bg-emerald-500"
+          } rounded-lg`}
+        >
+          {data.current.media_type === "image" ? (
+            <AiFillCamera className="mr-2" />
+          ) : (
+            <AiFillVideoCamera className="mr-2" />
+          )}
+          {data.current.media_type}
+        </div>
+        <h5 className="mb-2 text-xl font-semibold tracking-tight text-gray-900  capitalize ">
+          {data.current.note}
+        </h5>
+        <div className="flex">
+          <span className="inline-flex items-center py-1 px-2 text-sm justify-center text-white capitalize bg-orange-500 rounded-md">
+            {data.current.tags}
+          </span>
+        </div>
+        <DeleteMedia
+          open={deleteModalOpened}
+          handleClose={closeDeleteModalHandler}
+          id={id}
+          type={data.current.media_type}
+          handleShowClose={handleClose}
+        />
+      </Fragment>
+    );
+  }
 
-	return (
-		<Transition appear show={open} as={Fragment}>
-			<Dialog as="div" className="relative z-[200]" onClose={handleClose}>
-				<Transition.Child
-					as={Fragment}
-					enter="ease-out duration-300"
-					enterFrom="opacity-0"
-					enterTo="opacity-100"
-					leave="ease-in duration-200"
-					leaveFrom="opacity-100"
-					leaveTo="opacity-0"
-				>
-					<div className="fixed inset-0 bg-black bg-opacity-25" />
-				</Transition.Child>
+  return (
+    <Transition appear show={open} as={Fragment}>
+      <Dialog as="div" className="relative z-[200]" onClose={handleClose}>
+        <Transition.Child
+          as={Fragment}
+          enter="ease-out duration-300"
+          enterFrom="opacity-0"
+          enterTo="opacity-100"
+          leave="ease-in duration-200"
+          leaveFrom="opacity-100"
+          leaveTo="opacity-0"
+        >
+          <div className="fixed inset-0 bg-black bg-opacity-25" />
+        </Transition.Child>
 
-				<div className="fixed inset-0 overflow-y-auto">
-					<div className="flex min-h-full items-center justify-center md:p-4 text-center">
-						<Transition.Child
-							as={Fragment}
-							enter="ease-out duration-300"
-							enterFrom="opacity-0 scale-95"
-							enterTo="opacity-100 scale-100"
-							leave="ease-in duration-200"
-							leaveFrom="opacity-100 scale-100"
-							leaveTo="opacity-0 scale-95"
-						>
-							<Dialog.Panel className="w-full h-screen md:h-fit md:max-w-4xl md:w-11/12 transform overflow-hidden md:rounded-2xl bg-white py-6 px-3 md:px-6  text-left align-middle shadow-xl transition-all">
-								{content}
-							</Dialog.Panel>
-						</Transition.Child>
-					</div>
-				</div>
-			</Dialog>
-		</Transition>
-	);
+        <div className="fixed inset-0 overflow-y-auto">
+          <div className="flex min-h-full items-center justify-center md:p-4 text-center">
+            <Transition.Child
+              as={Fragment}
+              enter="ease-out duration-300"
+              enterFrom="opacity-0 scale-95"
+              enterTo="opacity-100 scale-100"
+              leave="ease-in duration-200"
+              leaveFrom="opacity-100 scale-100"
+              leaveTo="opacity-0 scale-95"
+            >
+              <Dialog.Panel className="w-full h-screen md:h-fit md:max-w-4xl md:w-11/12 transform overflow-hidden md:rounded-2xl bg-white py-6 px-3 md:px-6  text-left align-middle shadow-xl transition-all">
+                {content}
+              </Dialog.Panel>
+            </Transition.Child>
+          </div>
+        </div>
+      </Dialog>
+    </Transition>
+  );
 };
 export default ShowMedia;
