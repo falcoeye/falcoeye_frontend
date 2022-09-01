a167422f1f66801a1848b94f547acef6411ead0f
MostafaMofeed67 <mustafamufeed22@gmail.com>
2022-09-01 14:48:09 +0200

    CU-3g9mn3c - Feature, add validation for information fields in create analysis using react hook form & yup and show message in red text under the description

--- src/pages/Modals/AddAnalysis/components/steps/Informations.jsx
+++ src/pages/Modals/AddAnalysis/components/steps/Informations.jsx
@@ -1,6 +1,9 @@
 import { Fragment, useEffect, useRef } from "react";
 import { useMemo, useState } from "react";
 import "../../../Modals.css";
+import * as yup from "yup";
+import { Controller, useForm } from "react-hook-form";
+import { yupResolver } from "@hookform/resolvers/yup";
 
 const Informations = (props) => {
   const { params, updateData } = props;
@@ -51,21 +54,61 @@ const Informations = (props) => {
       return newState;
     });
   };
+
+  const schemaArray = questionFields.map((q) => ({
+    [q.name]: yup.number().required(),
+  }));
+  let yupSchema = {};
+  schemaArray.forEach((item, index) => {
+    let objKeys = Object.keys(item);
+    objKeys.forEach((key, index) => {
+      yupSchema[key] = item[key];
+    });
+  });
+
+  // form validation rules
+  const validationSchema = yup.object(yupSchema);
+
+  const {
+    formState: { errors },
+    control,
+  } = useForm({
+    resolver: yupResolver(validationSchema),
+    mode: "onChange",
+  });
+
   const renderedInputs = (
     <Fragment>
       {questionFields.map((field, index) => {
         const type = "text";
         return (
           <Fragment key={index}>
-            <input
-              type={type}
-              id={field.name}
-              className="analysis_form_input  dark:!bg-gray-800 dark:!border-gray-800 dark:!text-white !rounded-md"
+            <Controller
               name={field.name}
-              onChange={handleChange}
-              value={data[field.name]}
+              control={control}
+              render={({ field }) => (
+                <input
+                  {...field}
+                  type={type}
+                  id={field.name}
+                  value={data[field.name]}
+                  className="analysis_form_input  dark:!bg-gray-800 dark:!border-gray-800 dark:!text-white !rounded-md"
+                  onChange={(e) => {
+                    handleChange(e);
+
+                    field.onChange(e.target.value);
+                  }}
+                />
+              )}
             />
-            <p className=" w-full md:w-[85%] mt-1 mb-3 md:ml-[30px] text-sm text-orange-500">
+
+            {errors[field.name] && (
+              <p className=" w-full md:w-[85%] my-1  md:ml-[30px] font-semibold text-xs text-red-700">
+                This field must contains only numbers
+              </p>
+            )}
+
+            <p className=" w-full md:w-[85%] mb-3 md:ml-[30px] text-sm font-semibold text-orange-500">
               {field.disc}
             </p>
           </Fragment>
