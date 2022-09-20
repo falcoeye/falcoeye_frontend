47888589cc9da0d52d836e07408c32f510c3250f
Hossam <56669856+HOSS11H@users.noreply.github.com>
2022-09-20 04:16:49 +0200

    CU-3je84y7 - feature,  Remove buttons in video analysis (first,middle,last)

--- src/pages/Modals/ShowAnalysis/AnalysisFiles/MediaCarousel/MediaCarousel.jsx
+++ src/pages/Modals/ShowAnalysis/AnalysisFiles/MediaCarousel/MediaCarousel.jsx
@@ -7,7 +7,7 @@ import { useRef } from 'react';
 import { Fragment } from 'react';
 import { FaChevronLeft, FaChevronRight } from 'react-icons/fa';
 import { useState } from 'react';
-import Chip from '../../../../../Components/UI/Chip/Chip';
+// import Chip from '../../../../../Components/UI/Chip/Chip';
 
 const MediaCarousel = (props) => {
     const { files, id } = props;
@@ -26,7 +26,7 @@ const MediaCarousel = (props) => {
         setNextDisabled(swiper?.current?.isEnd)
     }
     
-    const firstClickHandler = () => {
+    /* const firstClickHandler = () => {
         swiper.current.slideTo(0)
         setPrevDisabled(swiper?.current?.isBeginning)
         setNextDisabled(swiper?.current?.isEnd)
@@ -40,7 +40,7 @@ const MediaCarousel = (props) => {
         swiper.current.slideTo(files.length)
         setPrevDisabled(swiper?.current?.isBeginning)
         setNextDisabled(swiper?.current?.isEnd)
-    }
+    } */
 
     let content = files.map((file, index) => {
         return (
@@ -56,11 +56,11 @@ const MediaCarousel = (props) => {
                 <button disabled={prevDisabled} className="shrink-0 bg-lime-500 hover:bg-lime-600 text-white  transition duration-300 font-bold p-2 rounded-full inline-flex items-center disabled:bg-gray-400 disabled:cursor-not-allowed" onClick={prevClickHandler} >
                     <FaChevronLeft />
                 </button>
-                <div className='grow hidden md:flex gap-3 justify-center' >
+                {/* <div className='grow hidden md:flex gap-3 justify-center' >
                     <Chip small active={prevDisabled} clickHandler={firstClickHandler} >first</Chip>
                     <Chip small active={Math.floor((files.length - 1) / 2) === swiper?.current?.activeIndex} clickHandler={middleClickHandler} >middle</Chip>
                     <Chip small active={nextDisabled}  clickHandler={lastClickHandler} >last</Chip>
-                </div>
+                </div> */}
                 <button disabled={nextDisabled} className="shrink-0 bg-lime-500 hover:bg-lime-600 text-white  transition duration-300 font-bold p-2 rounded-full inline-flex items-center disabled:bg-gray-400 disabled:cursor-not-allowed" onClick={nextClickHandler} >
                     <FaChevronRight />
                 </button>
