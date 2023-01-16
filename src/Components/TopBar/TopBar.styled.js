787d4908164be41c4cab89e1a5b66ae0b00d9122
HossamHelmyy <hossammohamedhelmy@gmail.com>
2023-01-16 17:18:57 +0200

    CU-8669amxza - enhancement, app header

--- src/Components/TopBar/TopBar.styled.js
+++ src/Components/TopBar/TopBar.styled.js
@@ -1,36 +1,41 @@
 import styled from "styled-components";
 export const TopBarContainer = styled.div`
-    position: fixed;
-    top: 0;
-    z-index: 101;
-    width: 100%;
-    padding: 4px 10px;
-    background-color: #161b22;
-    display: flex;
-    align-items: center;
-    justify-content: space-between;
-    padding: 4px 15px 8px 15px;
+	position: fixed;
+	top: 0;
+	z-index: 101;
+	width: 100%;
+	padding: 4px 10px;
+	background-color: #161b22;
+	display: flex;
+	align-items: center;
+	justify-content: space-between;
+	padding: 6px 15px 6px 15px;
 
-   & .MuiFormControl-root {
-    width: 180px;
-   }
-   & .MuiOutlinedInput-root {
-    height :42px;
-    font-size :14px;
-   }
+	& .MuiFormControl-root {
+		width: 180px;
+	}
+	& .MuiOutlinedInput-root {
+		height: 42px;
+		font-size: 14px;
+	}
 `;
 
 export const LogoContainer = styled.div`
-    width: 40px;
-    height: 40px;
+	width: 40px;
+	height: 40px;
 
-    img {
-        width: 100%;
-        height: 100%;
-    }
+	img {
+		width: 100%;
+		height: 100%;
+	}
 `;
 export const SelectWrapper = styled.div`
-    display :flex;
-    flex-direction: column;
-    gap :2px;
-`
+	display: flex;
+	align-items: center;
+	gap: 6px;
+	label {
+		@media (max-width: 600px) {
+			display: none;
+		}
+	}
+`;
